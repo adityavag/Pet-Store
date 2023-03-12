b599668faa7e58dff9c3c1625aee69c59fa63017
adi247u <adi247u@gmail.com>
2023-03-12 15:52:24 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-cyan-500'>
+      <h2>Puppies App</h2>
+    </div>
+  );
+}
+
+export default App;
