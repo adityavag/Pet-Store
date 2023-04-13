dfc81ceee7f92f0b4eea2996e15d5ccb6603e0cb
adi247u <adi247u@gmail.com>
2023-04-14 00:17:32 +0530

    :relieved Minor Fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import Pablo from '../images/pablo.jpeg'
 import {BsTrash} from 'react-icons/bs'
 import {RxUpdate} from 'react-icons/rx'
 
-export default function Card(props) {
+export default function Card({props}) {
   return (
 
 <div class="w-full max-w-sm rounded-lg shadow bg-gray-800 border-gray-700 m-5 text-white">
@@ -11,7 +11,7 @@ export default function Card(props) {
     </div>
     <div class="flex flex-col items-center pb-10">
         <img class="w-24 h-24 mb-3 rounded-full " alt={props.name} src={Pablo}/>
-        <h5 class="mb-1 text-xl font-semibold">{props.name}</h5>
+        <h5 class="mb-1 text-xl font-semibold">{props.id}</h5>
         <span class="text-sm font-semibold ">{props.name} is a {props.type}</span>
         <span class="text-sm font-semibold ">{props.name} is {props.age} old</span>
         <span class="text-sm font-semibold ">{props.name} is a {props.gender}</span>
